a32ca5ef4c54e789c858ba438cd7ddb2c40fb37c
samuelmohr <99822432+samuelmohr@users.noreply.github.com>
2022-02-16 18:09:10 +0100

    Update settings.py

--- src/covidtests/settings.py
+++ src/covidtests/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$rtotez4%!@16dh^px=hg+smp949%g-%nj3f9uiajzqrx*l5%!'
+SECRET_KEY = 'sosecret'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
